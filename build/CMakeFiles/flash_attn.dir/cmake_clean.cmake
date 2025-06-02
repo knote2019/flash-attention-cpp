bdb0f0ae4ee3dbc99a5bf8145eb383ad32784865
kasmvnc <kasmvnc@iluvatar.com>
2025-06-02 22:36:34 +0800

    fx

---
+++ build/CMakeFiles/flash_attn.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flash_attn.dir/src/api.cpp.o"
+  "CMakeFiles/flash_attn.dir/src/api.cpp.o.d"
+  "CMakeFiles/flash_attn.dir/src/flash-attention/flash_attn_generated_flash_fwd_hdim128_fp16_sm80.cu.o"
+  "CMakeFiles/flash_attn.dir/src/flash-attention/flash_attn_generated_flash_fwd_split_hdim128_fp16_sm80.cu.o"
+  "libflash_attn.pdb"
+  "libflash_attn.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/flash_attn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
