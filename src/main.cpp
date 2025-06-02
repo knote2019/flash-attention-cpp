4674c6b9c6db11c89e3c0826bd0b14a8c1f39040
kasmvnc <kasmvnc@iluvatar.com>
2025-06-03 00:42:59 +0800

    refactor5

--- src/main.cpp
+++ src/main.cpp
@@ -18,54 +18,13 @@ DEFINE_uint32(warmup_iterations, 1, "warmup iteration numbers and average the re
 DEFINE_uint32(profiling_iterations, 10, "profiling iteration numbers and average the result");
 DEFINE_uint32(sleep_duration, 100, "sleep_milliseconds between profiling");
 DEFINE_bool(enable_check, false, "check the GPU result against the CPU result");
-DEFINE_uint32(cpu_procs, omp_get_num_procs(), "processor num used of CPU");
 DEFINE_uint32(gpu_rank, 0, "the used GPU rank");
 
 int main(int argc, char *argv[]) {
     GFLAGS_NAMESPACE::ParseCommandLineFlags(&argc, &argv, true);
 
-    omp_set_num_threads(FLAGS_cpu_procs);
-    FAI_CHECK_CUDART_ERROR(cudaSetDevice(FLAGS_gpu_rank));
-
     cudaDeviceProp dev_prop;
     FAI_CHECK_CUDART_ERROR(cudaGetDeviceProperties(&dev_prop, FLAGS_gpu_rank));
-    FLOG("Flash Attention Inference start with %u CPU processes on the %u-th GPU: %s", FLAGS_cpu_procs, FLAGS_gpu_rank,
-         dev_prop.name);
-
-    int driver_version = 0;
-    int runtime_version = 0;
-    FAI_CHECK_CUDART_ERROR(cudaDriverGetVersion(&driver_version));
-    FAI_CHECK_CUDART_ERROR(cudaRuntimeGetVersion(&runtime_version));
-    FLOG("CUDA driver version / runtime version: %d.%d / %d.%d", driver_version / 1000, (driver_version % 100) / 10,
-         runtime_version / 1000, (runtime_version % 100) / 10);
-
-    FLOG("CUDA capability major/minor version number: %d.%d", dev_prop.major, dev_prop.minor);
-    FLOG("%d multiprocessors, %d CUDA cores/MP: %d CUDA cores", dev_prop.multiProcessorCount,
-         convert_SM_to_cores(dev_prop.major, dev_prop.minor),
-         convert_SM_to_cores(dev_prop.major, dev_prop.minor) * dev_prop.multiProcessorCount);
-    FLOG("GPU max clock rate: %.0f MHz (%0.2f GHz)", static_cast<double>(dev_prop.clockRate) * 1e-3,
-         static_cast<double>(dev_prop.clockRate) * 1e-6);
-    FLOG("Memory clock rate: %.0f MHz (%0.2f GHz)", static_cast<double>(dev_prop.memoryClockRate) * 1e-3,
-         static_cast<double>(dev_prop.memoryClockRate) * 1e-6);
-    FLOG("Memory bus width: %d-bit", dev_prop.memoryBusWidth);
-    FLOG("Total amount of global memory: %.0f MBytes (%zu Bytes)",
-         static_cast<double>(dev_prop.totalGlobalMem) / 1048576, dev_prop.totalGlobalMem);
-    FLOG("Total amount of constant memory: %.0f KBytes (%zu Bytes)", static_cast<double>(dev_prop.totalConstMem) / 1024,
-         dev_prop.totalConstMem);
-    FLOG("Total amount of shared memory per block: %.0f KBytes (%zu Bytes)",
-         static_cast<double>(dev_prop.sharedMemPerBlock) / 1024, dev_prop.sharedMemPerBlock);
-    FLOG("Total shared memory per multiprocessor: %.0f KBytes (%zu Bytes)",
-         static_cast<double>(dev_prop.sharedMemPerMultiprocessor) / 1024, dev_prop.sharedMemPerMultiprocessor);
-    FLOG("L2 cache size: %.0f KBytes (%d Bytes)", static_cast<double>(dev_prop.l2CacheSize) / 1024,
-         dev_prop.l2CacheSize);
-    FLOG("Total number of registers available per block: %d", dev_prop.regsPerBlock);
-    FLOG("Warp size: %d", dev_prop.warpSize);
-    FLOG("Max number of threads per multiprocessor: %d", dev_prop.maxThreadsPerMultiProcessor);
-    FLOG("Max number of threads per block: %d", dev_prop.maxThreadsPerBlock);
-    FLOG("Max dimension size of a thread block (x,y,z): (%d, %d, %d)", dev_prop.maxThreadsDim[0],
-         dev_prop.maxThreadsDim[1], dev_prop.maxThreadsDim[2]);
-    FLOG("Max dimension size of a grid size (x,y,z): (%d, %d, %d)", dev_prop.maxGridSize[0], dev_prop.maxGridSize[1],
-         dev_prop.maxGridSize[2]);
 
     cudaStream_t stream = nullptr;
 
